19e1a00a377748ef0ed0f5357ae6cd1fd4b03cb4
Etienne Santiago Samboni <etiennesamboni@gmail.com>
2023-12-06 18:49:51 -0500

    Se agregó la lógica básica con opciones estáticas del pc

---
+++ main.py
@@ -0,0 +1,27 @@
+user_option = input('piedra, papel o tijera => ')
+computer_option = 'papel' # Opción fija por el momento
+
+# Validaciones
+if user_option == computer_option :
+    print('Empate')
+elif user_option == 'piedra':
+    if computer_option == 'papel':
+        print('Papel gana a piedra')
+        print(f'El computador ganó')
+    else:
+        print('Piedra gana a tijera')
+        print('El user ganó!')
+elif user_option == 'papel':
+    if computer_option == 'piedra':
+        print('Papel gana a piedra')
+        print('El user ganó!')
+    else:
+        print('Tijera gana a papel')
+        print(f'El computador ganó')
+elif user_option == 'tijera':
+        if computer_option == 'piedra':
+            print('Piedra gana a tijera')
+            print(f'El computador ganó')
+        else:
+            print('Tijera gana a papel')
+            print('El user ganó!')
